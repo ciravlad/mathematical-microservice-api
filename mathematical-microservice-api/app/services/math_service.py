320b1ca8cacf1e0eb9e10b49710ac59ade46ae32
lbuzea <lorenabuzea25@yahoo.com>
2025-07-23 18:21:44 +0300

    feature(fibonacci): added view and service logic

--- mathematical-microservice-api/app/services/math_service.py
+++ mathematical-microservice-api/app/services/math_service.py
@@ -15,3 +15,16 @@ def compute_factorial(number: int) -> int:
     for i in range (2, number + 1):
         result *= i
     return result
+
+
+def compute_fibonacci(n: int) -> int:
+    if n < 0:
+        raise ValueError("n must be non-negative")
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
