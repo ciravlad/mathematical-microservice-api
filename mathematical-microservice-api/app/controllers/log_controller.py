a1e11e526971313c0813434ea4d9d85e7afae5b0
ciravlad <ciraionutvlad@gmail.com>
2025-07-30 18:00:17 +0300

    feature(logging) : Added implementation for log controller

---
+++ mathematical-microservice-api/app/controllers/log_controller.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..models.db_session import SessionLocal
+from ..models.request_log import RequestLog
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/logs")
+def get_logs(db: Session = Depends(get_db)):
+    return db.query(RequestLog).all()
