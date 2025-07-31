eb9f7ec5cd7e99c72c638fd742f544d97f8117db
ciravlad <ciraionutvlad@gmail.com>
2025-07-31 11:19:28 +0300

    feature(logging) : Added initialization for the databse

--- mathematical-microservice-api/app/main.py
+++ mathematical-microservice-api/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from .controllers.math_controller import router as math_router
+from app.models.db_session import init_db
 
 app = FastAPI(title="Math Microservice")
 app.include_router(math_router)
+init_db()
